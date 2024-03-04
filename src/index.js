b129c848b620f129afc6c066ef7c25c039d71031
dspodina <spodina.darya@gmail.com>
2024-03-04 09:04:15 +0100

    dependencies ran

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ const clearResult = document.getElementById('clear-btn');
 const calculateResult = document.getElementById('equal-sign');
 
 function appendToResult(value) {
-    resultValue.value += value;
+  resultValue.value += value;
 }
 
-clearResult.addEventListener('click', function (){
-    resultValue.value = '';
-}) 
+clearResult.addEventListener('click', function () {
+  resultValue.value = '';
+});
 
-calculateResult.addEventListener('click', function (){
-    resultValue.value = eval(resultValue.value);
-})
+calculateResult.addEventListener('click', function () {
+  resultValue.value = eval(resultValue.value);
+});
