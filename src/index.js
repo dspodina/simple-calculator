6ab3e86da95918948ca0ca9f1bb8567b5bdfa2e7
dspodina <spodina.darya@gmail.com>
2024-03-04 07:24:50 +0100

    simple calculator

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
-// Say hello let'us build a calculator
-alert("Hello there, let'us build a calculator");
+const resultValue = document.getElementById('result');
+const clearResult = document.getElementById('clear-btn');
+const calculateResult = document.getElementById('equal-sign');
+
+function appendToResult(value) {
+    resultValue.value += value;
+}
+
+clearResult.addEventListener('click', function (){
+    resultValue.value = '';
+}) 
+
+calculateResult.addEventListener('click', function (){
+    resultValue.value = eval(resultValue.value);
+})
